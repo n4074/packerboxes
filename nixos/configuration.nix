9bcb6537c7e538640ef1e065ede0590c0d23ea07
Carl Hattenfels <carl.hattenfels@gmail.com>
2020-05-15 23:40:55 +1000

    Simplify base image configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,12 +1,4 @@
 { config, pkgs, options, ... }:
-
-let 
-  unstable = builtins.fetchGit {
-    url = https://github.com/nixos/nixpkgs;
-    #sha256 = "0p98dwy3rbvdp6np596sfqnwlra11pif3rbdh02pwdyjmdvkmbvd";
-    ref = "master";
-  };
-in  
 {
   imports = [
     ./hardware-configuration.nix
@@ -14,26 +6,7 @@ in
 
   virtualisation.vmware.guest.enable = true;
 
-  nixpkgs.config = {
-    allowUnfree = true;
-
-    packageOverrides = pkgs: {
-      unstable = import unstable {
-        config = config.nixpkgs.config;
-      };
-    };
-
-  };
-
   system.stateVersion = "20.03";  
-
-  nix.nixPath =
-    # prepend default nixpath values.
-    options.nix.nixPath.default ++ 
-    # append our nixpkgs-overlays.
-    [ "nixpkgs-overlays=/etc/nixos/overlays/" ]
-  ;
-
 
   boot = {
     loader.systemd-boot.enable = true; # UEFI - switch to GRUB  if using BIOS
@@ -46,7 +19,10 @@ in
   services.sshd.enable = true;
 
   users.users.root = {
-    initialPassword = "triumvir egan stabile entice success"; # initially empty root password
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmOUt0rWo+d893BeoLp+ykyz225wRf8NUg23Mdfb5Y7" ];
+    #initialPassword = "triumvir egan stabile entice success";
+    openssh = {
+      passwordAuthentication = false;
+      authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmOUt0rWo+d893BeoLp+ykyz225wRf8NUg23Mdfb5Y7" ];
+    };
   };
 }
